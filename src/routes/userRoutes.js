ffe1a0978820255d3b37e710c791c8b72a4e9b29
Artem Shchirov <artemshchirov@gmail.com>
2022-09-11 20:52:54 +0300

    refactor

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,22 +1,13 @@
-const { celebrate, Joi } = require('celebrate');
-const userRoutes = require('express').Router();
+const { validateUserData } = require('../middlewares/validations');
+const userRouter = require('express').Router();
 
 const {
   getCurrentUser,
   updateProfile,
 } = require('../controllers/userControllers');
 
-userRoutes.get('/me', getCurrentUser);
+userRouter.get('/me', getCurrentUser);
 
-userRoutes.patch(
-  '/me',
-  celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().trim(true).email().required(),
-      name: Joi.string().trim(true).min(2).max(30).required(),
-    }),
-  }),
-  updateProfile
-);
+userRouter.patch('/me', validateUserData, updateProfile);
 
-module.exports = { userRoutes };
+module.exports = { userRouter };
